071b2095605e868ea864fee7693e63e423fadf1f
kenzie <108609750+kenzie-code@users.noreply.github.com>
2023-11-23 14:51:46 +0545

    Add files via upload

---
+++ HamroCarRental.API/BusinessLogic/AddStaffBL.cs
@@ -0,0 +1,34 @@
+﻿using HamroCarRental.Repository.IRepository;
+using HamroCarRental.Repository.Models;
+using HamroCarRental.Repository.Repository;
+using RootLibrary.ViewModels;
+
+namespace HamroCarRental.API.BusinessLogic
+{
+    public class AddStaffBL
+    {
+        private readonly IAddStaffRepo _addStaffRepo;
+        
+        public AddStaffBL(IAddStaffRepo addStaffRepo)
+        {
+            _addStaffRepo = addStaffRepo;
+        }
+
+        
+        public async Task<bool> AddNewStaff(StaffVM data)
+        {
+            var staff = new Staff
+            {
+                Name = data.Name,
+                Email = data.Email,
+                Address = data.Address,
+                Password = data.Password,
+                Position = data.Position,
+            };
+            
+            var result = await _addStaffRepo.AddStaff(staff);
+
+            return result;
+        }
+    }
+}
